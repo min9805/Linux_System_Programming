cc4ebb7b25262598e7cf99a330e8fcda30e42e52
min1925k <min1925k@gmail.com>
2023-01-03 14:36:47 +0900

    Synchronization

---
+++ 7_Synchroniization/prodcons_m.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <sys/time.h>
+#include "prodcons.h"
+
+BoundedBufferType	Buf;
+pthread_cond_t		NotFull;
+pthread_cond_t		NotEmpty;
+pthread_mutex_t		Mutex;
+
+void ThreadUsleep(int usecs)
+{
+	pthread_cond_t		cond;
+	pthread_mutex_t		mutex;
+	struct timespec		ts;
+	struct timeval		tv;
+
+	if (pthread_cond_init(&cond, NULL) < 0)  {
+		perror("pthread_cond_init");
+		pthread_exit(NULL);
+	}
+	if (pthread_mutex_init(&mutex, NULL) < 0)  {
+		perror("pthread_mutex_init");
+		pthread_exit(NULL);
+	}
+
+	gettimeofday(&tv, NULL);
+	ts.tv_sec = tv.tv_sec + usecs/1000000;
+	ts.tv_nsec = (tv.tv_usec + (usecs%1000000)) * 1000;
+	if (ts.tv_nsec >= 1000000000)  {
+		ts.tv_nsec -= 1000000000;
+		ts.tv_sec++;
+	}
+
+	if (pthread_mutex_lock(&mutex) < 0)  {
+		perror("pthread_mutex_lock");
+		pthread_exit(NULL);
+	}
+	if (pthread_cond_timedwait(&cond, &mutex, &ts) < 0)  {
+		perror("pthread_cond_timedwait");
+		pthread_exit(NULL);
+	}
+
+	if (pthread_cond_destroy(&cond) < 0)  {
+		perror("pthread_cond_destroy");
+		pthread_exit(NULL);
+	}
+	if (pthread_mutex_destroy(&mutex) < 0)  {
+		perror("pthread_mutex_destroy");
+		pthread_exit(NULL);
+	}
+}
+
+void Producer(void *dummy)
+{
+	int		i, data;
+	
+	printf("Producer: Start .... \n");
+	
+	for (i = 0; i < NLOOPS; i++)
+	{
+		pthread_mutex_lock(&Mutex);
+		while (Buf.counter == MAX_BUF){
+			pthread_cond_wait(&NotFull, &Mutex);
+		}
+		
+		printf("Producer: Producing an item... \n");
+		data = (rand()%100)*10000;
+		Buf.buf[Buf.in].data = data;
+		Buf.in = (Buf.in + 1) % MAX_BUF;
+		Buf.counter++;
+		
+		pthread_cond_signal(&NotEmpty);
+		pthread_mutex_unlock(&Mutex);
+
+		ThreadUsleep(data);
+	}
+}
+
+void Consumer(void *dummy)
+{
+	
+}
